df0bde76a758c164f5cef0416342062ea7231238
Isis van der Plas <isisvanderplas@hotmail.com>
2018-01-06 02:00:18 +0100

    empty input field after submit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const input = form.querySelector('input');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const text = input.value;
+  input.value = '';
   const ul = document.querySelector('#invitedList');
   const li = document.createElement('li');
   li.textContent = text;
